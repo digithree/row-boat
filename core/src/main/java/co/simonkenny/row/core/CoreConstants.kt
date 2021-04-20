e6ddd0cea1d32da97b0e4ecc3035f54e32dde90c
Simon <1c3k1ng@tutanota.com>
2021-04-20 14:28:06 +0100

    Hotfix, update ROW URL to private instance

--- core/src/main/java/co/simonkenny/row/core/CoreConstants.kt
+++ core/src/main/java/co/simonkenny/row/core/CoreConstants.kt
@@ -2,4 +2,4 @@ package co.simonkenny.row.core
 
 const val FILE_PROVIDER_AUTHORITY = "co.simonkenny.row.fileprovider"
 
-internal const val ROW_BASE_URL = "https://row.onl/"
+internal const val ROW_BASE_URL = "https://row-onl.herokuapp.com/"
